1832941695ce91e64eabfa354922c29aeaea0a86
Jack Rostron <jack@rostron.org.uk>
2014-01-12 20:28:53 +0000

    Created class files

---
+++ UIApplication+Permissions.h
@@ -0,0 +1,13 @@
+//
+//  UIApplication+Permissions.h
+//  UIApplication+Permissions Sample
+//
+//  Created by Jack Rostron on 12/01/2014.
+//  Copyright (c) 2014 Rostron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIApplication (Permissions)
+
+@end
---
+++ UIApplication+Permissions.m
@@ -0,0 +1,13 @@
+//
+//  UIApplication+Permissions.m
+//  UIApplication+Permissions Sample
+//
+//  Created by Jack Rostron on 12/01/2014.
+//  Copyright (c) 2014 Rostron. All rights reserved.
+//
+
+#import "UIApplication+Permissions.h"
+
+@implementation UIApplication (Permissions)
+
+@end
